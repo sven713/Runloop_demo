48df5fc709694c5040360964fab44519d58b8182
songximing <songximing@wtoip.com>
2018-06-13 18:17:23 +0800

    子线程的Runloop需要调用run方法手动启动，不会自动运行；如何退出子线程的运行循环？ nsthread exit, 主线程也是可以被退出的

---
+++ Runloop_demo/Runloop_demo/SVThread.h
@@ -0,0 +1,13 @@
+//
+//  SVThread.h
+//  Runloop_demo
+//
+//  Created by sve on 2018/6/13.
+//  Copyright © 2018年 sve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVThread : NSThread
+
+@end
---
+++ Runloop_demo/Runloop_demo/SVThread.m
@@ -0,0 +1,15 @@
+//
+//  SVThread.m
+//  Runloop_demo
+//
+//  Created by sve on 2018/6/13.
+//  Copyright © 2018年 sve. All rights reserved.
+//
+
+#import "SVThread.h"
+
+@implementation SVThread
+-(void)dealloc {
+    NSLog(@"子线程死了");
+}
+@end
