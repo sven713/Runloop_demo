49180102b5a1c3e7c4e8d1226ba886f1a9667f15
songximing <songximing@wtoip.com>
2018-06-13 17:05:40 +0800

    Runloop的三种模式default,trackingMode,commonMode

--- Runloop_demo/Runloop_demo/ViewController.m
+++ Runloop_demo/Runloop_demo/ViewController.m
@@ -17,10 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // timer不加入Runloop 不能运行
     NSTimer *timer = [NSTimer timerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
         NSLog(@"调用了定时器");
     }];
     
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode]; // 只加入defaulMode在拖动UI的时候,timer不会工作了
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:UITrackingRunLoopMode]; // 只加入这个mode,在不滑动的时候,timer不工作
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes]; //  commonMode 是上面两个mode的和,这一行代码等效上面两行
 }
 
 
